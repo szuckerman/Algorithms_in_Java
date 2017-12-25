85992e5af3b651b9d9198100818c158d34a75728
Sam Zuckerman <sam.p.zuckerman@gmail.com>
2017-12-25 00:14:22 -0500

    Added insertion_sort

---
+++ src/com/szuckerman/java_projects/My_Array.java
@@ -0,0 +1,54 @@
+package com.szuckerman.java_projects;
+import java.util.Arrays;
+
+public class My_Array {
+
+    private int[] my_array = {1, 3, 45, 2, 63, 10, 10, 22, 5, 222, 10, 20, 31, 100, 7, 4};
+
+//    public My_Array(){
+//        this.my_array = my_array;
+//    }
+
+    public String print_array(){
+        return Arrays.toString(my_array);
+    }
+
+    public String insertion_sort() {
+        //i is location to refer to
+        //k is the referrer
+        int i = 0;
+        int k = i + 1;
+        int temp;
+        while (k < my_array.length) {
+//            System.out.format("i: %d, k = %d\n", i, k);
+            if (my_array[k] >= my_array[i]) {
+                k += 1;
+                i += 1;
+//                System.out.println(Arrays.toString(my_array));
+//                System.out.format("i: key = %d, val = %d\n", i, my_array[i]);
+//                System.out.format("k: key = %d, val = %d\n", k, my_array[k]);
+            } else if (my_array[k] < my_array[i]) {
+
+                int val_to_move = my_array[k];
+
+//                System.out.format("We will be moving %d", val_to_move);
+//                System.out.println("");
+//                System.out.println("");
+
+                int new_k = k;
+
+                while (val_to_move < my_array[i]){
+                    my_array[new_k] = my_array[i];
+                    i -= 1;
+                    new_k -= 1;
+                }
+
+                my_array[i+1] = val_to_move;
+                k += 1;
+                i = k - 1;
+
+            }
+        }
+    return Arrays.toString(my_array);
+    }
+}
